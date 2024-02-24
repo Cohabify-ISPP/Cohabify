ac4593542570ee01ed2a04bc8c79992d38ae9699
darkgigi <daniel.arriaza.arriaza@gmail.com>
2024-02-24 19:46:34 +0100

    feat: Añadida lógica de detalles de piso

--- backend/src/main/java/org/ispp4/cohabify/advertisement/AdvertisementController.java
+++ backend/src/main/java/org/ispp4/cohabify/advertisement/AdvertisementController.java
@@ -1,10 +1,13 @@
 package org.ispp4.cohabify.advertisement;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,10 +23,20 @@ public class AdvertisementController {
         this.advertisementService = advertisementService;
     }
 
+    @Transactional(readOnly = true)
     @GetMapping("/advertisements")
     public ResponseEntity<List<Advertisement>> getAllAdvertisements() {
         List<Advertisement> advertisements = advertisementService.findAll();
         return new ResponseEntity<>(advertisements, HttpStatus.OK);
     }
+
+    @Transactional(readOnly = true)
+    @GetMapping("/advertisements/{id}")
+    public ResponseEntity<Advertisement> getAdvertisementById(ObjectId id) {
+        Optional<Advertisement> advertisement = advertisementService.findById(id);
+        return advertisement.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
 
 }
