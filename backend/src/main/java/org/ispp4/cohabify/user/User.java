6e9d9ac65738d5de9755785588785d8edd5eb481
miguel-angel-roldan-garcia <migrolgar2@alum.us.es>
2024-02-24 13:47:42 +0100

    feat: Addition of user authorities

--- backend/src/main/java/org/ispp4/cohabify/user/User.java
+++ backend/src/main/java/org/ispp4/cohabify/user/User.java
@@ -3,11 +3,15 @@ package org.ispp4.cohabify.user;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
+
 import org.ispp4.cohabify.model.BaseEntity;
 
 @Getter
@@ -48,6 +52,9 @@ public class User extends BaseEntity {
     
     @NotNull
     private Boolean is_verified;
+
+    @NotNull
+    private List<String> Authorities;
 
     public User() {
     
