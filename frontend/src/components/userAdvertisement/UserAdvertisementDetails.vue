2d50810ca0ae68ee7cf456df151762a28f558170
marmarsol4 <marmarsol4@alum.us.es>
2024-02-27 17:54:59 +0100

    research: Pantalla de detalles

---
+++ frontend/src/components/userAdvertisement/UserAdvertisementDetails.vue
@@ -0,0 +1,54 @@
+<script>
+import { ref, onMounted } from 'vue';
+
+export default {
+    
+    setup() {
+
+        //props = ['id'];
+        const userAdvertisement = ref({ description: "Desc de prueba", minBudget: 123.4, maxBudget: 567.8, desiredLocation: "Street 123", entranceDate: "2021-12-12", exitDate: "2022-12-12", maxCohabitants: 3});
+        /*
+        const fetchData = async () => {
+            try {
+                const response = await fetch(import.meta.env.VITE_API_URL + `/userAdvertisment/${id}`,
+                    {
+                        method: "GET",
+                        credentials: "include",
+                    });
+                const data = await response.json();
+
+                data.entranceDate = new Date(data.entranceDate);
+                data.exitDate = new Date(data.exitDate);
+                userAdvertisement.value = data;
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        };
+        */
+        
+
+        return {
+            userAdvertisement,
+        }
+    }
+}
+</script>
+
+<template>
+
+    <Navbar />
+
+    <div>
+        <p>{{ userAdvertisement.description }}</p>
+        <p>{{ userAdvertisement.minBudget }}</p>
+        <p>{{ userAdvertisement.maxBudget }}</p>
+        <p>{{ userAdvertisement.entranceDate }}</p>
+        <p>{{ userAdvertisement.exitDate }}</p>
+        <p>{{ userAdvertisement.maxCohabitants }}</p>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Main from './components/Main.vue';
 import Login from './components/authentication/Login.vue';
+import UserAdvertisementDetails from './components/userAdvertisement/UserAdvertisementDetails.vue';
 
 const routes = [
     { path: '/', component: Main },
-    { path: '/login', component: Login }
+    { path: '/login', component: Login },
+    {path: '/userAdvertisement', component: UserAdvertisementDetails},
 ];
   
 const router = createRouter({
