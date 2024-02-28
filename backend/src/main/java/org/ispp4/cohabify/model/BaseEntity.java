3963fe8fa301ccd58985ef6a84ed812c06169d3f
miguel-angel-roldan-garcia <migrolgar2@alum.us.es>
2024-02-29 00:18:29 +0100

    fix: Getter missing transient was producing bugs with jwt tokens

--- backend/src/main/java/org/ispp4/cohabify/model/BaseEntity.java
+++ backend/src/main/java/org/ispp4/cohabify/model/BaseEntity.java
@@ -1,12 +1,13 @@
 package org.ispp4.cohabify.model;
 
+import java.beans.Transient;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
 
 import jakarta.validation.constraints.NotNull;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,7 +18,8 @@ public class BaseEntity {
     @NotNull
     @Id
     protected ObjectId id;
-
+    
+    @Transient
     @JsonGetter("id")
     public String getJsonId() {
     	return this.id.toString();
