0821791a7f959b4d60b31d35b2c68abc3574dc60
olemornav <olemornav@alum.us.es>
2024-03-01 10:22:06 +0100

    fix: problemas de importación en BaseEntity

--- backend/src/main/java/org/ispp4/cohabify/model/BaseEntity.java
+++ backend/src/main/java/org/ispp4/cohabify/model/BaseEntity.java
@@ -1,13 +1,9 @@
 package org.ispp4.cohabify.model;
 
-import java.beans.Transient;
-
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import com.fasterxml.jackson.annotation.JsonGetter;
-
-import jakarta.validation.constraints.NotNull;
 
 import lombok.Getter;
 import lombok.Setter;
