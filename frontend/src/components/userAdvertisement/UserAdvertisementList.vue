62333ea74a7cabc69a683cc306936a64d4e57daf
marmarsol4 <marmarsol4@alum.us.es>
2024-03-08 00:23:51 +0100

    fix: Arreglado error de redireccionamiento

--- frontend/src/components/userAdvertisement/UserAdvertisementList.vue
+++ frontend/src/components/userAdvertisement/UserAdvertisementList.vue
@@ -301,7 +301,7 @@ export default {
       </div>
     </div>
     <div class="box" style="width:80vw" v-for="anuncio in userAds" :key="anuncio">
-      <a style="color: inherit; text-decoration: none; width:100%" :href="'/advertisements/users/' + anuncio?.id">
+      <div style="color: inherit; text-decoration: none; width:100%" @click="$router.push(`/advertisements/users/${anuncio.id}`)">
         <div class="inside-box" style="width:100%">
           <img class="imagen-circulo" :src="anuncio?.author?.imageUri" alt="Imagen del usuario">
           <div class="columna-informacion" style="width:100%">
@@ -319,7 +319,7 @@ export default {
             </span>
           </div>
         </div>
-      </a>
+      </div>
     </div>
   </div>
 </template>
