e6bd11e5963c7099ea99466b86ec35c156a28ce2
marmarsol4 <marmarsol4@alum.us.es>
2024-03-12 19:22:13 +0100

    feat: Implementado detalles de usuario

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -8,19 +8,22 @@ import HouseAdvertisementList from './components/houseAdvertisement/HouseAdverti
 import UserAdvertisementDetails from './components/userAdvertisement/UserAdvertisementDetails.vue';
 import CreateUserAdvertisement from './components/userAdvertisement/CreateUserAdvertisement.vue';
 import HouseAdvertisementDetails from './components/houseAdvertisement/HouseAdvertisementDetails.vue';
+import userDetails from './components/user/userDetails.vue';
 import Error from './components/Error.vue';
+
 
 const routes = [
 
     { name: "main", path: '/', component: Main },
     { path: '/login', component: Login },
     { path: '/register', component: Register },
-
+    
     { path: '/advertisements/users', component: UserAdvertisementList },
     { path: '/advertisements/houses', component: HouseAdvertisementList},
 
     { path: '/advertisements/users/:id', component: UserAdvertisementDetails },
     { path: '/advertisements/houses/:id', component: HouseAdvertisementDetails },
+    { path: "/user/:id", component: userDetails },
 
     { path: '/advertisements/users/new', component: CreateUserAdvertisement },
     { path: '/advertisements/houses/new', component: CreateHouseAdvertisement },
