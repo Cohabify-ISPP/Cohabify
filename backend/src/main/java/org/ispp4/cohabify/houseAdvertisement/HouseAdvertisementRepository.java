f8410d58ac2c78aa930253cd67e40819924a7d40
guigalde <guillermo.galeanodepaz@gmail.com>
2024-03-17 13:48:11 +0100

    fix: Faltaban @Repository en estas clases

--- backend/src/main/java/org/ispp4/cohabify/houseAdvertisement/HouseAdvertisementRepository.java
+++ backend/src/main/java/org/ispp4/cohabify/houseAdvertisement/HouseAdvertisementRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HouseAdvertisementRepository extends MongoRepository<HouseAdvertisement, ObjectId>{
     
     Optional<HouseAdvertisement> findById(ObjectId id);
