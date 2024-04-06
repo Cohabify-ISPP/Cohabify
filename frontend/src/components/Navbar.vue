e468ebda46306e279e3d1d4fad6c9ef102d339d8
Daniel Arriaza Arriaza <113770002+darkgigi@users.noreply.github.com>
2024-04-06 18:15:51 +0200

    fix: Cambiado botón de login
    Co-authored-by: Marina Calero <marcallop7@users.noreply.github.com>

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -184,16 +184,18 @@ watch(user, (newValue) => {
         </ul>
       </div>
     </div>
-    <div class="d-flex align-items-center" v-else style="padding-right: 1%;">
-      <a href="/login" class="text-decoration-none">  
-        <button class="login">
+    <div class="d-flex align-items-center" v-else style="padding-right: 1%;" >
+        <a href="/login" class="text-decoration-none">
+          <button style="width:100%; height: 10%;">
+              Iniciar sesión
+              <div class="arrow-wrapper">
+                  <div class="arrow"></div>
+
+              </div>
+          </button>
+        </a>
+      </div>
     
-          <div class="sign"><svg viewBox="0 0 512 512"><path d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z"></path></svg></div>
-          
-          <div class="text">Iniciar sesión</div>
-        </button>
-      </a>
-    </div>
   </div>
 </nav>
 </template>
@@ -220,71 +222,66 @@ watch(user, (newValue) => {
   transition: 0.2s;
 }
 
-.login {
+button {
+  --primary-color: #a4c7ff;
+  --secondary-color: #28426b;
+  --hover-color: #ffffff;
+  --arrow-width: 10px; 
+  --arrow-stroke: 2px; 
+  border: 0;
+  border-radius: 20px; 
+  color: var(--secondary-color);
+  padding: 0.5em 1.2em; 
+  background: var(--primary-color);
   display: flex;
+  transition: 0.2s background;
   align-items: center;
-  justify-content: flex-start;
-  width: 50px;
-  height: 50px;
-  border: none;
-  border-radius: 50%;
-  cursor: pointer;
-  position: relative;
-  overflow: hidden;
-  transition-duration: .3s;
-  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.199);
-  background-color: rgb(154, 171, 216);
+  gap: 0.3em; 
+  font-weight: bold;
 }
 
-/* plus sign */
-.sign {
-  width: 100%;
-  transition-duration: .3s;
+button .arrow-wrapper {
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
 }
 
-.sign svg {
-  width: 17px;
-}
-
-.sign svg path {
-  fill: white;
-}
-/* text */
-.text {
+button .arrow {
+  margin-top: 1px;
+  width: var(--arrow-width);
+  background: var(--primary-color);
+  height: var(--arrow-stroke);
   position: relative;
-  right: 0%;
-  width: 0%;
-  opacity: 0;
-  color: white;
-  font-size: 1.2em;
-  font-weight: 600;
-  transition-duration: .3s;
-}
-/* hover effect on button  width */
-.login:hover {
-  width: 200px;
-  border-radius: 40px;
-  transition-duration: .3s;
+  transition: 0.2s;
 }
 
-.login:hover .sign {
-  width: 30%;
-  transition-duration: .3s;
-  padding-left: 20px;
+button .arrow::before {
+  content: "";
+  box-sizing: border-box;
+  position: absolute;
+  border: solid var(--secondary-color);
+  border-width: 0 var(--arrow-stroke) var(--arrow-stroke) 0;
+  display: inline-block;
+  top: -3px; 
+  right: 3px; 
+  transition: 0.2s;
+  padding: 3px; 
+  transform: rotate(-45deg);
+  font-weight: bold;
+    
 }
-/* hover effect button's text */
-.login:hover .text {
-  opacity: 1;
-  width: 70%;
-  transition-duration: .3s;
-  padding-right: 10px;
+
+button:hover {
+  background-color: var(--hover-color);
 }
-/* button click effect*/
-.login:active {
-  transform: translate(2px ,2px);
+
+button:hover .arrow {
+  background: var(--secondary-color);
 }
+
+button:hover .arrow:before {
+  right: 0;
+}
+
 
 </style>
