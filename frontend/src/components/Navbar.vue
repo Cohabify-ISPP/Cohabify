a180845075cac8acc049cab658d5010471af2173
Daniel Arriaza Arriaza <113770002+darkgigi@users.noreply.github.com>
2024-04-06 17:13:18 +0200

    feat: Mejorado botón de inicio de sesión
    Co-authored-by: Marina Calero <marcallop7@users.noreply.github.com>

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -184,8 +184,15 @@ watch(user, (newValue) => {
         </ul>
       </div>
     </div>
-    <div class="d-flex align-items-center" v-else>
-      <a href="/login" class="btn btn-primary">Iniciar sesión</a>
+    <div class="d-flex align-items-center" v-else style="padding-right: 1%;">
+      <a href="/login" class="text-decoration-none">  
+        <button class="login">
+    
+          <div class="sign"><svg viewBox="0 0 512 512"><path d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z"></path></svg></div>
+          
+          <div class="text">Iniciar sesión</div>
+        </button>
+      </a>
     </div>
   </div>
 </nav>
@@ -211,6 +218,73 @@ watch(user, (newValue) => {
 .dropdown:hover::after {
   border-top-color: #a4c7ff;
   transition: 0.2s;
+}
+
+.login {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 50px;
+  height: 50px;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition-duration: .3s;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.199);
+  background-color: rgb(154, 171, 216);
+}
+
+/* plus sign */
+.sign {
+  width: 100%;
+  transition-duration: .3s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.sign svg {
+  width: 17px;
+}
+
+.sign svg path {
+  fill: white;
+}
+/* text */
+.text {
+  position: relative;
+  right: 0%;
+  width: 0%;
+  opacity: 0;
+  color: white;
+  font-size: 1.2em;
+  font-weight: 600;
+  transition-duration: .3s;
+}
+/* hover effect on button  width */
+.login:hover {
+  width: 200px;
+  border-radius: 40px;
+  transition-duration: .3s;
+}
+
+.login:hover .sign {
+  width: 30%;
+  transition-duration: .3s;
+  padding-left: 20px;
+}
+/* hover effect button's text */
+.login:hover .text {
+  opacity: 1;
+  width: 70%;
+  transition-duration: .3s;
+  padding-right: 10px;
+}
+/* button click effect*/
+.login:active {
+  transform: translate(2px ,2px);
 }
 
 </style>
