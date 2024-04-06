a67d404d044b4940f4ba625cf89ffbea6212c534
olemornav <olemornav@alum.us.es>
2024-04-06 14:28:38 +0200

    fix: añadir secretkey a properties

--- backend/src/main/java/org/ispp4/cohabify/order/OrderController.java
+++ backend/src/main/java/org/ispp4/cohabify/order/OrderController.java
@@ -7,6 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.LineItemCollection;
@@ -18,11 +21,18 @@ import org.springframework.http.HttpStatus;
 @RestController
 public class OrderController {
 
+    @Autowired
+    private Environment environment;
+
     @PostMapping("/api/stripe/session")
     public ResponseEntity<Map<String, String>> handleStripeSession(@RequestBody Map<String,String> requestBody) throws StripeException {
         String sessionId = requestBody.get("sessionId");
         // Configura tu clave secreta de Stripe
-        Stripe.apiKey = "sk_test_51P2DTpBofFRUNSKs1qVLmShlrmOTeWgjLo6w91bjKvS0PAjOnJgmvly7aUq6tNQndH8YYhI3KCT5894MIwwwStS800uAcksgZF";
+
+        
+        String secretKey = environment.getProperty("stripe.secret.key"); 
+
+        Stripe.apiKey = secretKey;
     
         // Utiliza el sessionId para obtener los detalles del pago desde Stripe
         Session session = Session.retrieve(sessionId);
