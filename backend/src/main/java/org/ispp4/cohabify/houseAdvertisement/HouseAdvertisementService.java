5ac826a0365089c63aa05174eb5d2f1e33951860
guigalde <73796037+guigalde@users.noreply.github.com>
2024-04-07 14:55:11 +0200

    fix: Corchete que faltaba

--- backend/src/main/java/org/ispp4/cohabify/houseAdvertisement/HouseAdvertisementService.java
+++ backend/src/main/java/org/ispp4/cohabify/houseAdvertisement/HouseAdvertisementService.java
@@ -77,7 +77,8 @@ public class HouseAdvertisementService {
         }
         return advertisements;
 
-
+    }
+    
     public HouseAdvertisement findAdvertisementByHouseId(ObjectId id) {
         return advertisementRepository.findAdvertisementByHouseId(id).get();
 
