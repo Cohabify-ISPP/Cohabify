951658ab3fb9a2c5dbb7a2c31fe0fe4f763c168c
marmarsol4 <marmarsol4@alum.us.es>
2024-04-14 14:16:16 +0200

    fix: Arreglado test end-to-end login
    Co-authored-by: Ismael Barrera García <TerryIsmael@users.noreply.github.com>

--- frontend/src/test/components/loginTest.js
+++ frontend/src/test/components/loginTest.js
@@ -26,8 +26,12 @@ const login = async() => {
     await driver.findElement(By.id('username')).sendKeys('javgaragu90', Key.RETURN);
     await driver.findElement(By.id('password')).sendKeys('javgaragu90', Key.RETURN);
     await driver.findElement(By.className('btn-primary')).click();
-    await driver.wait(until.elementLocated(By.id('loginModal')), 1000);
-  } finally {
+    await driver.wait(async () => {
+      const element = await driver.findElement(By.id("loginModal"));
+      const displayStyle = await element.getCssValue('display');
+      return displayStyle === 'block';
+    }, 10000);
+  }finally {
     await driver.quit();
   }
 };
@@ -37,28 +41,23 @@ const socialLogin = async() => {
   try {
     await driver.get('http://localhost:5173/login');
     await driver.findElement(By.id("bypass")).click();
-    const newUrl = await driver.getCurrentUrl();
+    
+    await driver.wait(until.urlContains("/register"), 10000);
+    await driver.wait(until.elementLocated(By.name('name')), 10000).getAttribute('required') !== null;
+    await driver.wait(until.elementLocated(By.name('username')), 10000).getAttribute('required') !== null;
+    await driver.wait(until.elementLocated(By.name('phone')), 10000).getAttribute('required') !== null;
+    
+    const emailInput = await driver.wait(until.elementLocated(By.name('email')), 10000);
+    const emailValue = await emailInput.getAttribute('value').then(text=> text.match('.*@.*\..*'));
+    const emailDisabled = await emailInput.getAttribute('readonly');
 
-    if (newUrl.includes("/register")) {
-      await driver.wait(until.elementLocated(By.name('name')), 10000).getAttribute('required') !== null;
-      await driver.wait(until.elementLocated(By.name('username')), 10000).getAttribute('required') !== null;
-      await driver.wait(until.elementLocated(By.name('phone')), 10000).getAttribute('required') !== null;
-      
-      const emailInput = await driver.wait(until.elementLocated(By.name('email')), 10000);
-      const emailValue = await emailInput.getAttribute('value').then(text=> text.match('.*@.*\..*'));
-      const emailDisabled = await emailInput.getAttribute('readonly');
-  
-      if (!emailValue) {
-        throw new Error("El correo no se ha rellenado correctamente")
-      } else if (!emailDisabled) {
-        throw new Error("El correo no está deshabilitado")
-      }
-
-
-    } else {
-      throw new Error("No se ha redireccionado correctamente en el login social")
+    if (!emailValue) {
+      throw new Error("El correo no se ha rellenado correctamente")
+    } else if (!emailDisabled) {
+      throw new Error("El correo no está deshabilitado")
     }
-
+  } catch (e) {
+    throw new Error("Error en el login social: " + e);
   } finally {
     await driver.quit();
   }
--- frontend/src/test/test.js
+++ frontend/src/test/test.js
@@ -1,4 +1,12 @@
 import loginTest from './components/loginTest.js';
 
-await loginTest();
+console.log("Running tests");
+try{
+    
+    await loginTest();
+    console.log("[OK] All tests passed successfully.");
+} catch(e) {
+    console.error("[ERROR] Tests failed. See the error trace here:\n ", e);
+}
+
 process.exit(0);
