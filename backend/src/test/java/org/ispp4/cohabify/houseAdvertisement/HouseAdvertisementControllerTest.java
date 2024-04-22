4e65188b86977ba5e5246ee77665bdab576d200b
miguel-angel-roldan-garcia <migrolgar2@alum.us.es>
2024-04-22 15:54:26 +0200

    fix: Arreglado test para cumplir con cambios en develop

--- backend/src/test/java/org/ispp4/cohabify/houseAdvertisement/HouseAdvertisementControllerTest.java
+++ backend/src/test/java/org/ispp4/cohabify/houseAdvertisement/HouseAdvertisementControllerTest.java
@@ -160,8 +160,8 @@ public class HouseAdvertisementControllerTest {
         request.setHouse(advertisement3.getHouse());
         request.setHouseId(house3.getId());
         request.setImagesB(List.of());
-        request.setX(2.);
-        request.setY(2.);
+        request.setX(2f);
+        request.setY(2f);
 
         when(advertisementService.checkPromotions(anyList())).thenAnswer(invocation -> invocation.getArgument(0));
 
