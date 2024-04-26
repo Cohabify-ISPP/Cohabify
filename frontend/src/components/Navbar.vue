a73f5f6b9b4269a66dfb66bf500556e9587084d7
Daniel Arriaza Arriaza <113770002+darkgigi@users.noreply.github.com>
2024-04-26 10:58:14 +0200

    fix: Corregidos nombres en la navbar para clarificar partes de aplicación
    Co-authored-by: Marina Calero <marcallop7@users.noreply.github.com>

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -126,6 +126,7 @@ watch(user, (newValue) => {
         </li>
         <li class="nav-item dropdown pr-4" style="padding-right: 1%;">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Buscar
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
           </svg>
@@ -138,7 +139,7 @@ watch(user, (newValue) => {
        
         <li class="nav-item dropdown" v-if="isLoggedIn">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Anuncios de vivienda
+            Mis anuncios de vivienda
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <li><a class="dropdown-item" @click.prevent="$router.push('/myAdvertisements/house')">Ver publicados</a></li>
@@ -149,7 +150,7 @@ watch(user, (newValue) => {
 
         <li class="nav-item dropdown" v-if="isLoggedIn">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Anuncios de compañero
+            Mi anuncio como compañero
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <li><a class="dropdown-item" @click.prevent="findMyUserAd">Ver publicado</a></li>
